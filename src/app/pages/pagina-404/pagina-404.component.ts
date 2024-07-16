274a822e86d69fef8aa82c1135845b9cee16cadd
Diego Mejías <mejias.diego19@gmail.com>
2024-07-16 10:36:40 +0200

    Grana Service

--- src/app/pages/pagina-404/pagina-404.component.ts
+++ src/app/pages/pagina-404/pagina-404.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { GranaService } from '../../servicios/grana.service';
 
 @Component({
   selector: 'app-pagina-404',
@@ -8,4 +9,15 @@ import { RouterModule } from '@angular/router';
   templateUrl: './pagina-404.component.html',
   styleUrl: './pagina-404.component.css',
 })
-export class Pagina404Component {}
+export class Pagina404Component {
+  constructor(private GranaService: GranaService) {}
+  ngOnInit() {
+    this.GranaService.detectKonamiCode().subscribe(() => {
+      window.open(
+        'https://www.youtube.com/watch?v=UNSuzoHduXk',
+        '_blank',
+        'width=600,height=400'
+      );
+    });
+  }
+}
